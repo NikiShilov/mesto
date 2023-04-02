7eac254116f82f09ecd03349bc481c92d55b2b75
Nikita Shilov <niki.shilov@yandex.ru>
2023-04-02 18:03:02 +0300

    for review

--- scripts/index.js
+++ scripts/index.js
@@ -5,32 +5,29 @@ let profileText = profile.querySelector('.profile__text');
 let popup = document.querySelector('.popup');
 let popupContainer = popup.querySelector('.popup__container');
 let popupClose = popupContainer.querySelector('.popup__close');
-let popupSave = popupContainer.querySelector('.popup__savebutton');
-let nameInput = popupContainer.querySelector('.popup__name')
-let jobInput = popupContainer.querySelector('.popup__text')
+let nameInput = popupContainer.querySelector('.popup__input_type_name');
+let jobInput = popupContainer.querySelector('.popup__input_type_text');
+let cards = document.querySelector('.cards');
 function openPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileText.textContent;
 }
 
-profileEdit.addEventListener('click', openPopup);
-
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-popupClose.addEventListener('click', closePopup);
-
-
 function handleFormSubmit(evt) {
     evt.preventDefault();
-
     profileName.textContent = nameInput.value;
     profileText.textContent = jobInput.value;
     closePopup();
 }
 
+profileEdit.addEventListener('click', openPopup);
 
-popupContainer.addEventListener('submit', handleFormSubmit);
+popupClose.addEventListener('click', closePopup);
+
+form.addEventListener('submit', handleFormSubmit);
 
